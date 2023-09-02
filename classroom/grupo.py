1828765678f69ef038c75968c7f9179383d1401e
Nicolas Gomez Echeverri <nigomeze@unal.edu.co>
2023-09-02 14:24:32 -0500

    pls work
    entrega 1

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,45 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+            if self._asignaturas == None:
+                self._asignaturas = [Asignatura(x)]
+            else:
+                self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, lista=None):
+        if lista == None and self.listadoAlumnos == None:
+            self.listadoAlumnos = [alumno]
+        elif lista != None and self.listadoAlumnos == None:
+            lista.append(alumno)
+            self.listadoAlumnos = lista
+        elif lista == None and self.listadoAlumnos != None:
+            self.listadoAlumnos.append(alumno)
+        elif lista != None and self.listadoAlumnos != None:
+            lista.append(alumno)
+            self.listadoAlumnos += lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return "Grupo de estudiantes: " + self._grupo
 
-    @ classmethod
+    """@ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+        cls.grado = nombre"""
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
+    """@ classmethod
     def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+        cls.grado = nombre"""
+
+
